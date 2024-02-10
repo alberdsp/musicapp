55e4e26a0e27a058b348e869dd75e37edad73df7
Alber <alberdsp@gmail.com>
2024-02-10 19:31:30 +0100

    insertadas imagenes y paso a cada instrumento

--- lib/main.dart
+++ lib/main.dart
@@ -5,94 +5,129 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  // aplicacion principal
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplicación xilofono',
+      title: 'Aplicación Xilófono',
       theme: ThemeData(
-          // tema de la aplicacion
-          primarySwatch: Colors.purple),
-      home: const MyHomePage(title: 'Aplicación de sonidos de xilofono'),
+        primarySwatch: Colors.indigo,
+      ),
+      home: const MyHomePage(title: 'My sound app'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  // widget de la pagina principal
+// Clase principal
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  // Pantalla principal
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+        title: Text(title),
+      ), // Barra superior de la app
+
+      // Cuerpo de la app con tres botones
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            // Botones con imágenes de los instrumentos
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => XilofonoForm()),
+                );
+              },
+              child: Image.asset('assets/media/xilofono.png',
+                  width: 150, height: 150),
+            ), // botón detección de gestos
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BongosForm()),
+                );
+              },
+              child: Image.asset('assets/media/bongos.png',
+                  width: 150, height: 150),
+            ), // botón detección de gestos
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => GuitarraForm()),
+                );
+              },
+              child: Image.asset('assets/media/guitarra.png',
+                  width: 150, height: 150),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+/**
+ * Clase para las pantalla del xilófono
+ */
+
+class XilofonoForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tocar Xilófono'),
+      ),
+      body: Center(
+        child: Text('Esta es la pantalla del xilófono'),
+      ),
+    );
+  }
+}
+
+/**
+ * Clase para las pantalla de los bongos
+ */
+
+class BongosForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tocar Bongos'),
+      ),
+      body: Center(
+        child: Text('Esta es la pantalla de los bongos'),
+      ),
+    );
+  }
+}
+
+/**
+ * Clase para las pantalla de la guitarra
+ */
+
+class GuitarraForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tocar Guitarra'),
+      ),
+      body: Center(
+        child: Text('Esta es la pantalla de la guitarra'),
+      ),
     );
   }
 }
